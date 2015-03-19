9a074714a819762f30422a3628a919931a91c451
Nicolas Chambrier <naholyr@gmail.com>
2015-03-19 17:36:34 +0100

    Initial release

---
+++ index.js
@@ -0,0 +1,321 @@
+"use strict";
+
+/* eslint-env node */
+/* eslint no-use-before-define:0 */
+
+var _ = require("lodash");
+
+
+module.exports = {
+  "parse": parse,
+  "stringify": stringify,
+  "item": todoItem
+};
+
+
+var DEFAULT_ENCODING = "utf8";
+
+var RE_COMPLETE = /^x\s+/;
+var RE_PRIORITY = /^\(([A-Z])\)\s+/;
+var RE_DATE = /^(\d{4}-\d{2}-\d{2})\s+/;
+
+var RE_CONTEXT_AND_PROJECT = /\s([@\+])(\S+)/g;
+
+
+function parse (lines, encoding) {
+  if (Array.isArray(lines)) {
+    return lines.map(function (line) {
+      return parseLine(line, encoding || DEFAULT_ENCODING);
+    });
+  }
+
+  if (Buffer.isBuffer(lines)) {
+    return parse(lines.toString(encoding || DEFAULT_ENCODING));
+  }
+
+  return parse(lines.trim().split(/\r?\n/));
+}
+
+function parseLine (line, encoding) {
+  if (Buffer.isBuffer(line)) {
+    line = line.toString(encoding || DEFAULT_ENCODING);
+  }
+
+  line = line.trim();
+
+  if (!line) {
+    return null;
+  }
+
+  var props = {
+    "complete": false,
+    "completeDate": null,
+    "priority": "",
+    "date": null, // Date
+    "text": ""
+  };
+
+  // Complete?
+  line = regexTrimLeft(line, RE_COMPLETE, function () {
+    props.complete = true;
+  });
+
+  if (props.complete) {
+    // Completion date?
+    line = regexTrimLeft(line, RE_DATE, function (m) {
+      props.completeDate = parseDate(m[1]);
+      props.prout = true;
+    });
+  }
+
+  // Priority?
+  line = regexTrimLeft(line, RE_PRIORITY, function (m) {
+    props.priority = m[1];
+  });
+
+  // Date?
+  line = regexTrimLeft(line, RE_DATE, function (m) {
+    props.date = parseDate(m[1]);
+  });
+
+  // Text = whatever remains
+  props.text = line;
+
+  return todoItem(props);
+}
+
+function todoItem (props) {
+  // Extract contexts and projects
+  var cp = parseContextsAndProjects(props.text || "");
+
+  var item = {
+    "complete": Boolean(props.complete),
+    "completeDate": (!props.completeDate || (props.completeDate instanceof Date)) ? props.completeDate : parseDate(props.completeDate),
+    "priority": props.priority || "",
+    "date": (!props.date || (props.date instanceof Date)) ? props.date : parseDate(props.date),
+    "text": props.text || "",
+    "contexts": cp.contexts,
+    "projects": cp.projects,
+    // Methods
+    "addContext": addContext,
+    "removeContext": removeContext,
+    "addProject": addProject,
+    "removeProject": removeProject
+  };
+
+  function addContext (context) {
+    if (item.contexts.indexOf(context) === -1) {
+      item.contexts.push(context);
+      item.text = addTag(item.text, "@" + context);
+    }
+  }
+
+  function addProject (project) {
+    if (item.projects.indexOf(project) === -1) {
+      item.projects.push(project);
+      item.text = addTag(item.text, "+" + project);
+    }
+  }
+
+  function removeContext (context) {
+    if (item.contexts.indexOf(context) !== -1) {
+      item.contexts = _.without(item.contexts, context);
+      item.text = removeTag(item.text, "@" + context);
+    }
+  }
+
+  function removeProject (project) {
+    if (item.projects.indexOf(project) !== -1) {
+      item.projects = _.without(item.projects, project);
+      item.text = removeTag(item.text, "@" + project);
+    }
+  }
+
+  return item;
+}
+
+function addTag (text, tag) {
+  if ((text + " ").indexOf(" " + tag + " ") === -1) {
+    text += " " + tag;
+  }
+
+  return text;
+}
+
+function removeTag (text, tag) {
+  var found;
+  do {
+    found = (text + " ").indexOf(" " + tag + " ");
+    if (found !== -1) {
+      text = text.substring(0, found) + text.substring(found + tag.length + 1);
+    }
+  } while (found !== -1);
+
+  return text;
+}
+
+
+function regexMatchAll (string, pattern) {
+  var matches = [];
+
+  var current;
+  do {
+    current = pattern.exec(string);
+    if (current) {
+      matches.push(current);
+    }
+  } while (current);
+
+  return matches;
+}
+
+function parseDate (string) {
+  return new Date(string);
+}
+
+function regexTrimLeft (string, pattern, onMatch) {
+  var match = string.match(pattern);
+
+  if (match) {
+    onMatch(match);
+
+    // Trim
+    string = string.substring(match[0].length);
+  }
+
+  return string;
+}
+
+function parseContextsAndProjects (string) {
+  var contexts = [];
+  var projects = [];
+
+  // Allow starting with a context or project
+  regexMatchAll(" " + string, RE_CONTEXT_AND_PROJECT).forEach(function (match) {
+    if (match[1] === "@") {
+      if (contexts.indexOf(match[2]) === -1) {
+        contexts.push(match[2]);
+      }
+    } else if (match[1] === "+") {
+      if (projects.indexOf(match[2]) === -1) {
+        projects.push(match[2]);
+      }
+    }
+  });
+
+  return {
+    "contexts": contexts,
+    "projects": projects
+  };
+}
+
+
+function stringify (items) {
+  if (Array.isArray(items)) {
+    return items.map(stringifyItem).join("\n");
+  } else if (typeof items === "object" && isItemsList(items)) {
+    return stringify(itemsListAsArray(items));
+  } else if (typeof items === "object" && isItem(items)) {
+    return stringifyItem(items);
+  } else {
+    throw new Error("Unexpected input: should be a TodoTxt item or list of TodoTxt items");
+  }
+}
+
+function stringifyItem (item) {
+  if (!item) {
+    return null;
+  }
+
+  var line = "";
+
+  // Complete?
+  if (item.complete) {
+    line += "x ";
+
+    // Completion date?
+    if (item.completeDate) {
+      line += stringifyDate(item.completeDate) + " ";
+    }
+  }
+
+  // Priority?
+  if (item.priority) {
+    line += "(" + item.priority + ") ";
+  }
+
+  // Date?
+  if (item.date) {
+    line += stringifyDate(item.date) + " ";
+  }
+
+  // Main content
+  line += item.text;
+
+  return line;
+}
+
+function isItemsList (object) {
+  return Object.keys(object).every(function isItemListKey (key) {
+    // Key must be a number
+    if (!isNaN(Number(key))) {
+      return false;
+    }
+    // Value must be an item
+    return isItem(object[key]);
+  });
+}
+
+function isItem (object) {
+  return typeof object === "object"
+      && "complete" in object
+      && "completeDate" in object
+      && "priority" in object
+      && "date" in object
+      && "text" in object
+      && "contexts" in object
+      && "projects" in object;
+}
+
+function itemsListAsArray (object) {
+  var result = [];
+
+  // copy object values as array values
+  for (var key in object) {
+    result[Number(key)] = object;
+  }
+
+  // fill gaps with null
+  for (var i = 0; i < result.length; i++) {
+    if (!result[i]) {
+      result[i] = null;
+    }
+  }
+
+  return result;
+}
+
+function stringifyDate (date) {
+  if (typeof date === "string") {
+    return date;
+  }
+
+  if (!(date instanceof Date)) {
+    throw new Error("Invalid date value");
+  }
+
+  var yyyy = String(date.getFullYear());
+
+  var mm = String(date.getMonth() + 1);
+  if (mm.length === 1) {
+    mm = "0" + mm;
+  }
+
+  var dd = String(date.getDate());
+  if (dd.length === 1) {
+    dd = "0" + dd;
+  }
+
+  return yyyy + "-" + mm + "-" + dd;
+}
+
---
+++ test/index.js
@@ -0,0 +1,154 @@
+"use strict";
+
+/* eslint-env node */
+/* globals describe:0, it:0 */
+
+var todotxt = require("../");
+
+var expect = require("expect");
+
+
+describe("TodoTxt", function () {
+
+  describe("Parser", function () {
+
+    it("should parse line", function () {
+      var items = todotxt.parse("Some text");
+      expect(items).toBeAn(Array);
+      expect(items.length).toEqual(1);
+      var item = items[0];
+      expect(item).toBeAn(Object);
+      expect(item.text).toEqual("Some text");
+    });
+
+    it("should define default values", function () {
+      var item = todotxt.parse("Some text")[0];
+      // Default values
+      expect(item.date).toEqual(null);
+      expect(item.complete).toEqual(false);
+      expect(item.completeDate).toEqual(null);
+      expect(item.priority).toEqual("");
+      expect(item.contexts).toEqual([]);
+      expect(item.projects).toEqual([]);
+    });
+
+    it("should expose methods to handle projects & contexts", function () {
+      var item = todotxt.parse("Some text")[0];
+      // Default values
+      expect(item.addProject).toBeA(Function);
+      expect(item.removeProject).toBeA(Function);
+      expect(item.addContext).toBeA(Function);
+      expect(item.removeContext).toBeA(Function);
+    });
+
+    it("should parse completed task", function () {
+      var item = todotxt.parse("x 2015-03-20 2015-03-21 Some text")[0];
+      expect(item.text).toEqual("Some text");
+      expect(item.date).toBeA(Date);
+      expect(item.date.getDate()).toEqual(21);
+      expect(item.complete).toEqual(true);
+      expect(item.completeDate).toBeA(Date);
+      expect(item.completeDate.getDate()).toEqual(20);
+    });
+
+    it("should parse task with priority", function () {
+      var item = todotxt.parse("(A) 2015-03-21 Some text")[0];
+      expect(item.priority).toEqual("A");
+    });
+
+    it("should parse task with everything", function () {
+      var item = todotxt.parse("x 2015-03-20 (B) 2015-03-21 Some text with @Context and @Context and again @OtherContext attached to a +Project")[0];
+      expect(item.text).toEqual("Some text with @Context and @Context and again @OtherContext attached to a +Project");
+      expect(item.priority).toEqual("B");
+      expect(item.date.getDate()).toEqual(21);
+      expect(item.complete).toEqual(true);
+      expect(item.completeDate.getDate()).toEqual(20);
+      expect(item.contexts).toEqual(["Context", "OtherContext"]);
+      expect(item.projects).toEqual(["Project"]);
+    });
+
+    it("should parse multiple lines", function () {
+      var items = todotxt.parse("x Complete task\nIncomplete task\nx (A) Complete task with priority\nIncomplete task with +Project");
+      expect(items.length).toEqual(4);
+      expect(items[0].text).toEqual("Complete task");
+      expect(items[0].complete).toEqual(true);
+      expect(items[1].text).toEqual("Incomplete task");
+      expect(items[1].complete).toEqual(false);
+      expect(items[2].text).toEqual("Complete task with priority");
+      expect(items[2].priority).toEqual("A");
+      expect(items[3].text).toEqual("Incomplete task with +Project");
+      expect(items[3].complete).toEqual(false);
+    });
+
+    it("should keep empty lines", function () {
+      var items = todotxt.parse("Task 1\n\nTask 2");
+      expect(items.length).toEqual(3);
+      expect(items[0].text).toEqual("Task 1");
+      expect(items[2].text).toEqual("Task 2");
+    });
+
+  });
+
+  describe("Stringifier", function () {
+
+    var item;
+
+    it("should create item", function () {
+      item = todotxt.item({
+        "text": "Hello @Context"
+      });
+    });
+
+    it("should parse context from text", function () {
+      expect(item.contexts).toEqual(["Context"]);
+    });
+
+    it("should complete task", function () {
+      item.complete = true;
+      expect(todotxt.stringify(item)).toEqual("x Hello @Context");
+    });
+
+    it("should append added contexts", function () {
+      item.addContext("Context1");
+      item.addContext("Context2");
+      expect(todotxt.stringify(item)).toEqual("x Hello @Context @Context1 @Context2");
+    });
+
+    it("should remove contexts", function () {
+      item.removeContext("Context");
+      expect(todotxt.stringify(item)).toEqual("x Hello @Context1 @Context2");
+    });
+
+    it("should add project", function () {
+      item.addProject("SayHello");
+      expect(todotxt.stringify(item)).toEqual("x Hello @Context1 @Context2 +SayHello");
+    });
+
+    it("should set completion date", function () {
+      item.completeDate = new Date("2015-03-20");
+      expect(todotxt.stringify(item)).toEqual("x 2015-03-20 Hello @Context1 @Context2 +SayHello");
+    });
+
+    it("should set task date too", function () {
+      item.date = new Date("2015-03-18");
+      expect(todotxt.stringify(item)).toEqual("x 2015-03-20 2015-03-18 Hello @Context1 @Context2 +SayHello");
+    });
+
+    it("should set priority", function () {
+      item.priority = "X";
+      expect(todotxt.stringify(item)).toEqual("x 2015-03-20 (X) 2015-03-18 Hello @Context1 @Context2 +SayHello");
+    });
+
+    it("shoud remove all contexts", function () {
+      item.contexts.forEach(item.removeContext);
+      expect(todotxt.stringify(item)).toEqual("x 2015-03-20 (X) 2015-03-18 Hello +SayHello");
+    });
+
+    it("should uncomplete task (completion date should disappear)", function () {
+      item.complete = false;
+      expect(todotxt.stringify(item)).toEqual("(X) 2015-03-18 Hello +SayHello");
+    });
+
+  });
+
+});
